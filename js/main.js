a130716df653d1959936daec2c0aae87b7566a14
KALININ ALEXEY <2257855@gmail.com>
2021-01-31 05:35:26 +0300

    4

--- js/main.js
+++ js/main.js
@@ -24,8 +24,23 @@
   
 const swiper = new Swiper('.swiper-container', {
     loop: true,
-    slidesPerView: '2',
+    slidesPerView: '1',
     navigation: {
       nextEl: '.arrow'
-    }
+    },
+    breakpoints: {
+        540: {
+          slidesPerView: 2,
+        }
+      }
   });
+
+  
+
+
+var menuButton = document.querySelector('.menu-button');
+var menu = document.querySelector('.header');
+menuButton.addEventListener('click', function () {
+    menuButton.classList.toggle('menu-button-active');
+    menu.classList.toggle('header-active');
+})
