230408e6d186d5a93833c21493c2952ebe67e124
KALININ ALEXEY <2257855@gmail.com>
2021-02-06 17:05:38 +0300

    1

--- js/main.js
+++ js/main.js
@@ -1,41 +1,34 @@
-const swiper = new Swiper('.swiper-container', {
-    loop: true,
-    slidesPerView: '1',
-    navigation: {
-      nextEl: '.arrow'
+const swiper = new Swiper(".swiper-container", {
+  loop: true,
+  slidesPerView: "1",
+  navigation: {
+    nextEl: ".arrow",
+  },
+  breakpoints: {
+    540: {
+      slidesPerView: 2,
     },
-    breakpoints: {
-        540: {
-          slidesPerView: 2,
-        }
-      }
-  });
+  },
+});
 
-  
-const menuButton = document.querySelector('.menu-button');
-const header = document.querySelector('.header');
+const menuButton = document.querySelector(".menu-button");
+const header = document.querySelector(".header");
 
-menuButton.addEventListener('click', event => {
-    const target = event.target;
-    
-   
-      if (!menuButton.classList.contains('menu-button-active')) { 
-        header.classList.add('header-active');
-        menuButton.classList.add('menu-button-active');
-      } else {
-        menuButton.classList.remove('menu-button-active');
-        header.classList.add('header-hiding');
-        setTimeout(() => {
-          header.classList.remove('header-active');
-          header.classList.remove('header-hiding');
-        }, 400)
-      }
-    
-  });
+menuButton.addEventListener("click", (event) => {
+  const target = event.target;
 
-
-
-
+  if (!menuButton.classList.contains("menu-button-active")) {
+    header.classList.add("header-active");
+    menuButton.classList.add("menu-button-active");
+  } else {
+    menuButton.classList.remove("menu-button-active");
+    header.classList.add("header-hiding");
+    setTimeout(() => {
+      header.classList.remove("header-active");
+      header.classList.remove("header-hiding");
+    }, 400);
+  }
+});
 
 // var menuButton = document.querySelector('.menu-button');
 // var header = document.querySelector('.header');
@@ -43,5 +36,3 @@ menuButton.addEventListener('click', event => {
 //   menuButton.classList.toggle('menu-button-active');
 //   header.classList.toggle('header-active');
 // })
-
-
